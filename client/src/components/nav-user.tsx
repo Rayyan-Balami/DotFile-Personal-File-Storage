b1dd8c923c0c24b654d2a53d7df7717db71c65c7
Your Name <visionrayyan60517@gmail.com>
2025-06-06 19:39:07 +0545

    style: capitalize user name in NavUser component

--- client/src/components/nav-user.tsx
+++ client/src/components/nav-user.tsx
@@ -77,7 +77,7 @@ export function NavUser() {
               <AvatarFallback className="rounded-lg">{initials}</AvatarFallback>
             </Avatar>
             <div className="grid flex-1 text-left text-sm leading-tight">
-              <span className="truncate font-medium">{user.name}</span>
+              <span className="truncate font-medium capitalize">{user.name}</span>
               <span className="truncate text-xs">{user.email}</span>
             </div>
             <ChevronsUpDown className="ml-auto" />
